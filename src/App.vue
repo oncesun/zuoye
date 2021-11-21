7b0914670e79dda29643370e9870ee1ea6f17302
oncesun <93325533+oncesun@users.noreply.github.com>
2021-11-21 15:14:18 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <zuoye />
+  </div>
+</template>
+<script>
+import zuoye from "./components/zuoye.vue";
+export default {
+  components: {
+    zuoye,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/zuoye.vue
@@ -0,0 +1,472 @@
+<template>
+  <div class="father">
+    <body>
+      <head>
+        <title>Microsoft Edge Inside</title>
+      </head>
+      <div class="dao">
+        <div class="box1">
+          <div class="head">
+            <img src="zuoye\src\img 作业3\作业4.9.png" width="108px" height="23px" />
+          </div>
+          <a href="#" class="choice">|</a>
+          <a href="#" class="choice">Microsoft Edge Inside</a>
+          <a href="#" class="choice1">下载</a>
+          <a href="#" class="choice1">最新动态</a>
+          <a href="#" class="choice1">下一步</a>
+          <a href="#" class="choice1">大型企业</a>
+          <a href="#" class="choice1">社区</a>
+          <a href="#" class="choice1">支持</a>
+        </div>
+        <div class="box2">
+          <a href="#" class="choice1">所有Microsoft</a>
+          <a href="#" class="search"
+            ><img src="zuoye\src\img 作业3\搜索，猫客.png" width="20px" height="20px"
+          /></a>
+        </div>
+      </div>
+      <div class="text1">
+        <div class="introduce1">
+          <h1 class="title1">查看即将推出的内容！</h1>
+          <p class="para1">
+            Microsoft Edge 预览频道现已推出移动版！
+            请访问下载页面，根据你的移动设备选择要安装的版本。
+            想成为第一批预览新增功能的人吗？ 我们将持续更新包含最新功能的
+            Insider Channels，请立即下载并加入 Microsoft Edge 预览体验成员社区。
+          </p>
+          <a class="link1" href="#"
+            ><p class="textlink1">下载Beta渠道</p>
+            <p class="textlink2">Windows 10/11版</p></a
+          >
+          <a href="#" class="link2">更多平台和渠道</a>
+        </div>
+        <div class="img1"><img src="zuoye\src\img 作业3\作业4.1.png" /></div>
+      </div>
+      <div class="img2">
+        <div class="plan">
+          <div class="a">
+            <div class="img3"><img src="zuoye\src\img 作业3\作业4.2.png" /></div>
+          </div>
+          <div class="text2">
+            <div class="plan1">
+              <p class="para2">现已推出</p>
+              <p class="para3">加入 Microsoft Edge 移动版预览体验计划</p>
+              <a class="link3" href="#">更多移动版频道</a>
+            </div>
+          </div>
+          <div class="text3">
+            <div class="img4">
+              <img src="zuoye\src\img 作业3\作业4.3.png" />
+              <p class="para4">Android 版 Beta 频道</p>
+              <img src="zuoye\src\img 作业3\下载.png" />
+            </div>
+            <div class="plan2"><img src="zuoye\src\img 作业3\作业4.4.png" /></div>
+          </div>
+          <div class="text4">
+            <div class="img5">
+              <img src="zuoye\src\img 作业3\作业4.5.png" />
+              <p class="para4">iOS 版 Insider Channels</p>
+              <p class="para5">当前向新用户开放</p>
+              <a href="#" class="link4"
+                ><p class="textlink3">下载Beta渠道</p>
+                <p class="textlink4">iOS版</p></a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="introduce">
+      <div class="introduce2">
+        <div class="text5">
+          <h2 class="title2">成为 Microsoft Edge 预览体验成员</h2>
+          <p class="para6">
+            我们的预览体验成员社区在帮助构建 Microsoft Edge
+            方面发挥了重大作用，而且我们的创新从未停止。
+            如果你想要了解我们的后续工作，我们诚邀你下载我们的预览渠道并加入预览体验成员社区。
+            届时你将可以抢先预览新功能，并且有机会在开发过程中与 Microsoft Edge
+            团队合作。
+            你的反馈和想法可以帮助我们改进，因此我们希望你能加入我们。
+            单击下载以开始使用！
+          </p>
+          <div class="img7"><img src="zuoye\src\img 作业3\作业4.6.png" /></div>
+        </div>
+      </div>
+      <div class="introduce2">
+        <div class="text5">
+          <div class="img7"><img src="zuoye\src\img 作业3\作业4.7.png" /></div>
+          <h2 class="title3">为大众打造更好的互联网空间</h2>
+          <p class="para6">
+            我们在 Microsoft Edge 的开发过程中采用了 Chromium
+            开源项目，以此来为我们的客户提供更好的网站兼容性，并为所有的网站开发人员降低网站的碎片化程度。
+            我们将继续为 Chromium 项目做出积极贡献。 到目前为止，我们已经兑现了
+            1,700
+            多项承诺，并期待继续积极参与社区互动，为大众打造更好的互联网空间。
+            如需详细了解我们的贡献，请参阅 GitHub 上的 Microsoft Edge
+            “Explainers”，并查看我们的源代码发布。
+          </p>
+        </div>
+      </div>
+      <div class="introduce2">
+        <div class="text5">
+          <h2 class="title2">查看 Microsoft Edge Insider Channels</h2>
+          <p class="para6">
+            我们提供三个预览渠道：Beta、Dev 和 Canary。
+            这些频道适用于所有受支持版本的 Windows、Windows Server 以及 macOS。
+            Beta 版频道可以提供最稳定的预览版体验，每 4 周更新一次。
+            开发频道每周更新一次，稳定性略逊于 Beta 版，但优于 Canary。
+            对于想要在相对稳定的版本内获取最新更新的人员，Dev 是一个不错的选择。
+            如果你想要查看我们昨天的工作并获取每日更新，那么 Canary
+            可能是你的理想预览渠道。 安装预览渠道不需要卸载 Microsoft Edge
+            的已发布版本，你可以同时安装多个版本。
+            若要详细了解不同的渠道并开始使用，请转到 下载页。
+          </p>
+          <div class="img7"><img src="zuoye\src\img 作业3\作业4.10.png" /></div>
+        </div>
+      </div>
+      <div class="introduce2">
+        <div class="text5">
+          <div class="img7"><img src="zuoye\src\img 作业3\作业4.11.png" /></div>
+          <h2 class="title3">我们乐于倾听！</h2>
+          <p class="para6">
+            Microsoft Edge 团队希望倾听你的声音。
+            在使用预览渠道时，请务必通过工具栏上的“发送反馈”按钮来分享你的反馈。
+            你还可以访问我们的 Microsoft Edge 预览体验成员论坛或在 Twitter
+            上关注 @MSEdgeDev。
+            我们会定期分享来自预览体验成员社区的热门反馈总结，以便你可以了解最受期待的功能、最新出现的问题以及你的反馈对产品产生的影响。
+            衷心希望你加入社区。如果尚未加入，我们期待倾听你的想法！
+          </p>
+        </div>
+      </div>
+      </div>
+      <div class="text">
+        <ul class="last1">
+          <li class="include1">新增内容</li>
+          <li><a href="#" class="a">Microsoft 365</a></li>
+          <li><a href="#" class="a">Surface Laptop 2</a></li>
+          <li><a href="#" class="a">Surface Go</a></li>
+          <li><a href="#" class="a">Windows 11 应用程序</a></li>
+        </ul>
+        <ul class="last1">
+          <li class="include1">Microsofe Store</li>
+          <li><a href="#" class="a">账户个人资料</a></li>
+          <li><a href="#" class="a">下载中心</a></li>
+          <li><a href="#" class="a">订单跟踪</a></li>
+        </ul>
+        <ul class="last1">
+          <li class="include1">教育</li>
+          <li><a href="#" class="a">Microsoft 教育领域</a></li>
+          <li><a href="#" class="a">适合学生的 Office</a></li>
+          <li><a href="#" class="a">适用于学校的 Office 365</a></li>
+        </ul>
+        <ul class="last1">
+          <li class="include1">企业</li>
+          <li><a href="#" class="a">Azure</a></li>
+          <li><a href="#" class="a">AppSource</a></li>
+          <li><a href="#" class="a">汽车</a></li>
+          <li><a href="#" class="a">政府机构</a></li>
+          <li><a href="#" class="a">医疗保健</a></li>
+          <li><a href="#" class="a">制造业</a></li>
+          <li><a href="#" class="a">金融服务</a></li>
+          <li><a href="#" class="a">零售业</a></li>
+        </ul>
+        <ul class="last1">
+          <li class="include1">开发人员</li>
+          <li><a href="#" class="a">Microsoft Visual Studio</a></li>
+          <li><a href="#" class="a">Windows 开发人员中心</a></li>
+          <li><a href="#" class="a">开发人员中心</a></li>
+          <li><a href="#" class="a">Microsoft 开发人员计划</a></li>
+          <li><a href="#" class="a">第9频道</a></li>
+          <li><a href="#" class="a">Microsoft 365 开发人员中心</a></li>
+          <li><a href="#" class="a">Microsoft 365 开发人员计划</a></li>
+        </ul>
+        <ul class="last1">
+          <li class="include1">公司</li>
+          <li><a href="#" class="a">招贤纳士</a></li>
+          <li><a href="#" class="a">关于 Microsoft</a></li>
+          <li><a href="#" class="a">公司新闻</a></li>
+          <li><a href="#" class="a">Microsoft 隐私</a></li>
+          <li><a href="#" class="a">投资人</a></li>
+          <li><a href="#" class="a">安全性</a></li>
+        </ul>
+      </div>
+      <div class="thanks">
+        <div><a href="#" class="lang">中文（中国）</a></div>
+        <div class="thanks1">
+          <div><a href="#" class="long">与 Microsoft 联系</a></div>
+          <div><a href="#" class="long">隐私</a></div>
+          <div><a href="#" class="long">使用条款</a></div>
+          <div><a href="#" class="long">商标</a></div>
+          <div><a href="#" class="long">关于我们的广告</a></div>
+          <div><a href="#" class="long"> 京ICP备09042378号-6</a></div>
+          <div><a href="#" class="long"> 京公网安备 11010802023178</a></div>
+          <div class="lang">© Microsoft 2021</div>
+        </div>
+      </div>
+    </body>
+  </div>
+</template>
+<style scoped>
+.dao {
+  display: flex;
+  height: 54px;
+  justify-content: space-between;
+  width: 90%;
+  margin: 0 40px;
+}
+.head {
+  width: 116px;
+  height: 44px;
+  margin-right: 10px;
+}
+.choice {
+  text-decoration: none;
+  color: #262626;
+  font-weight: 600;
+  font-size: 16px;
+}
+.choice1 {
+  text-decoration: none;
+  color: #262626;
+  font-size: 13px;
+  padding-top: 2px;
+}
+.choice:hover,
+.choice1:hover {
+  text-decoration: underline;
+}
+.box1 {
+  display: flex;
+  justify-content: space-around;
+  width: 680px;
+  margin: 10px;
+}
+.box2 {
+  width: 400px;
+  display: flex;
+  margin: 10px;
+  justify-content: space-around;
+}
+.father {
+  margin: 0 auto;
+}
+.text1 {
+  margin: 0 auto;
+  width: 90%;
+  overflow: hidden;
+  height: 320px;
+}
+.introduce1 {
+  width: 642px;
+  margin: 0 auto;
+  height: 280px;
+  display: inline-block;
+}
+.para1 {
+  text-align: center;
+  font-size: 18px;
+}
+.img1 {
+  display: inline-block;
+  margin: 0 auto;
+  width: 420px;
+  text-align: center;
+  height: 280px;
+}
+.title1 {
+  font-size: 40px;
+  text-align: center;
+}
+.link1 {
+  text-decoration: none;
+  color: white;
+  text-align: center;
+  margin: 0 auto;
+  background-color: #0077d9;
+  width: 312px;
+  height: 66px;
+  display: block;
+  overflow: hidden;
+}
+.link1:hover {
+  background-color: #0267b8;
+}
+.textlink1 {
+  margin-top: 10px;
+  font-weight: 600;
+  font-size: 18px;
+}
+.textlink2 {
+  font-size: 14px;
+  margin-top: -14px;
+}
+.link2 {
+  text-align: center;
+  text-decoration: none;
+  margin: 10px auto;
+  width: 312px;
+  display: block;
+  color: #0074b9;
+}
+.link2:hover {
+  text-decoration: underline;
+}
+.img2 {
+  background-color: #243a5e;
+  width: 100%;
+  height: 400px;
+  margin-top: 40px;
+}
+.plan {
+  display: flex;
+  justify-content: space-around;
+  width: 1140px;
+  margin: 0 auto;
+  height: 400px;
+  overflow: hidden;
+}
+.para2 {
+  background-color: #fdb814;
+  display: flex;
+  width: 72px;
+  height: 24px;
+  font-size: 12px;
+  line-height: 24px;
+  overflow: hidden;
+  border-radius: 5%;
+  font-weight: 600;
+  padding-left: 20px;
+}
+.para3 {
+  color: white;
+  font-size: 24px;
+}
+.link3 {
+  color: white;
+}
+.link3:hover {
+  color: #0077d9;
+}
+.para4 {
+  color: white;
+  font-weight: 600;
+  font-size: 18px;
+}
+.para5 {
+  color: white;
+  font-weight: 600;
+  font-size: 14px;
+}
+.textlink3 {
+  font-weight: 600;
+  font-size: 18px;
+  margin-top: 10px;
+}
+.textlink4 {
+  margin-top: -10px;
+}
+.link4 {
+  text-decoration: none;
+  color: white;
+  text-align: center;
+  margin: 0 auto;
+  background-color: #0077d9;
+  width: 200px;
+  height: 66px;
+  display: block;
+  overflow: hidden;
+  border-radius: 2%;
+}
+.link4:hover {
+  background-color: #0267b8;
+}
+.introduce{
+  background-color: #F6F6F6;
+  width: 100%;
+}
+.introduce2 {
+  margin: 0 auto;
+  height: 230px;
+  width: 1148px;
+  background-color: #F6F6F6;
+}
+.img7 {
+  display: inline-block;
+}
+.para6 {
+  width: 700px;
+  margin-top: -10px;
+  display: inline-block;
+}
+.title2 {
+  margin-top: 48px;
+  font-size: 28px;
+  display: inline-block;
+}
+.title3 {
+  margin-top: 48px;
+  font-size: 28px;
+  display: inline-block;
+  width: 600px;
+}
+.text {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  height: 350px;
+  margin-top: 48px;
+  background-color: white;
+}
+.include1 {
+  font-size: 15px;
+  line-height: 32px;
+  color: #616161;
+  font-weight: bold;
+  text-decoration: none;
+}
+.a {
+  text-decoration: none;
+  font-size: 11px;
+  color: #616161;
+  line-height: 30px;
+}
+.a:hover {
+  text-decoration: underline;
+}
+.last1 {
+  width: 20%;
+}
+li {
+  list-style: none;
+}
+.thanks {
+  width: 100%;
+  height: 32px;
+  margin: 0 auto;
+  display: flex;
+  background-color:white;
+  justify-content: space-around;
+}
+.lang {
+  text-decoration: none;
+  font-size: 11px;
+  color: #616161;
+  line-height: 30px;
+}
+.lang:hover {
+  text-decoration: underline;
+}
+.long {
+  text-decoration: none;
+  font-size: 11px;
+  color: #616161;
+  line-height: 30px;
+}
+.long:hover {
+  text-decoration: underline;
+}
+.thanks1 {
+  display: flex;
+  justify-content: space-around;
+  width: 80%;
+}
+</style>
